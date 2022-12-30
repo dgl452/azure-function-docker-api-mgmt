aabaf3a3bf56a6c43823ab8435b35590d71331c1
Daniel González <daniel.gonzalez@global.ntt>
2022-12-30 17:46:29 +0100

    Added unauth requests

--- authenticator/__init__.py
+++ authenticator/__init__.py
@@ -1,15 +1,24 @@
 import logging
-
 import azure.functions as func
+from random import randrange
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # ALL THE LOGIC TO VALIDATE THE USER GO HERE
+    # WE ARE CREATING A RANDOM TO SIMULTE BAD REQUESTS
 
-    return func.HttpResponse(
-             "the user looks good",
-             status_code=200
-        )
+    good_token = randrange(2)
+
+    if good_token == 0:
+        return func.HttpResponse(
+                "the user looks good",
+                status_code=200
+            )
+    else:
+        return func.HttpResponse(
+                "bad user or password",
+                status_code=401
+            )
 
